ea13aef88ad865138e9e1ef57f62c529821d59b7
d1xn9uf <zaryprepa@gmail.com>
2025-01-15 15:04:33 +0100

    Update sort_five.c

--- sort/sort_five.c
+++ sort/sort_five.c
@@ -6,51 +6,46 @@
 /*   By: mzary <mzary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 01:04:54 by mzary             #+#    #+#             */
-/*   Updated: 2025/01/08 08:07:27 by mzary            ###   ########.fr       */
+/*   Updated: 2025/01/15 15:02:30 by mzary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort.h"
 
-static int	get_pos(t_stack *stack_a, int rank);
+static int	is_sorted(t_stack *stack_a);
 static void	push_head(t_stack *stack_a, t_stack *stack_b);
 static void	push_two(t_stack *stack_a, t_stack *stack_b);
+static int	get_pos(t_stack *stack_a, int rank);
 
 void	sort_five(t_stack *stack_a, t_stack *stack_b)
 {
-	while (stack_a->size > 3)
-		push_b(stack_a, stack_b, 1);
-	sort_three(stack_a);
-	if (stack_b->size == 1)
-		push_head(stack_a, stack_b);
-	else
-		push_two(stack_a, stack_b);
+	if (!is_sorted(stack_a))
+	{
+		while (stack_a->size > 3)
+			push_b(stack_a, stack_b, 1);
+		sort_three(stack_a);
+		if (stack_b->size == 1)
+			push_head(stack_a, stack_b);
+		else
+			push_two(stack_a, stack_b);
+	}
 }
 
-static int	get_pos(t_stack *stack_a, int rank)
+static int	is_sorted(t_stack *stack_a)
 {
-	int		pos;
-	int		h_rank;
 	int		i;
 	t_node	*node_i;
 
-	if (rank < get_min_rank(stack_a) || rank > get_max_rank(stack_a))
-		return (rank_index(stack_a, get_min_rank(stack_a)));
 	i = 0;
 	node_i = stack_a->head;
-	pos = rank_index(stack_a, get_max_rank(stack_a));
-	h_rank = get_max_rank(stack_a);
 	while (i < stack_a->size)
 	{
-		if (node_i->rank > rank && node_i->rank < h_rank)
-		{
-			h_rank = node_i->rank;
-			pos = i;
-		}
+		if (node_i->rank != i)
+			return (0);
 		i += 1;
 		node_i = node_i->next;
 	}
-	return (pos);
+	return (1);
 }
 
 static void	push_head(t_stack *stack_a, t_stack *stack_b)
@@ -94,4 +89,30 @@ static void	push_two(t_stack *stack_a, t_stack *stack_b)
 		push_head(stack_a, stack_b);
 		push_head(stack_a, stack_b);
 	}
+}
+
+static int	get_pos(t_stack *stack_a, int rank)
+{
+	int		pos;
+	int		h_rank;
+	int		i;
+	t_node	*node_i;
+
+	if (rank < get_min_rank(stack_a) || rank > get_max_rank(stack_a))
+		return (rank_index(stack_a, get_min_rank(stack_a)));
+	i = 0;
+	node_i = stack_a->head;
+	pos = rank_index(stack_a, get_max_rank(stack_a));
+	h_rank = get_max_rank(stack_a);
+	while (i < stack_a->size)
+	{
+		if (node_i->rank > rank && node_i->rank < h_rank)
+		{
+			h_rank = node_i->rank;
+			pos = i;
+		}
+		i += 1;
+		node_i = node_i->next;
+	}
+	return (pos);
 }
